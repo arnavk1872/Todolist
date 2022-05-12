2e3b6ea4af9f7fdd7a469f19778f3fd3687fb7f9
Arnav khajuria <arnavk1802@gmail.com>
2022-05-12 16:53:34 +0530

    Procfile added & Changes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://arnavk1872:Cenation00!@cluster0.rvxor.mongodb.net/todolistDB");
 
 const itemsSchema = {
   name: String
@@ -135,6 +135,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT, function() {
   console.log("Server started on port 3000");
 });
